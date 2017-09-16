ab790f937606e2cf3964fa914a8cc39752477a8b
F915 <iamboyuyang@163.com>
2017-09-16 16:56:04 +0800

    delete security key

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ import qrcode
 
 from PIL import  Image
 
-APPID = "2017032906469430"
+APPID = 
 
-private_key = "-----BEGIN RSA PRIVATE KEY-----\nMIICXQIBAAKBgQC2flDu4ojUYDFy/FL+BvLIU37fShBtqJHcamw8yv4Zqd2YV0oQVIyFf/mT+qlKoJmzKeFMucWV3fp6Z3hrxRmzIksBCm+Q+L/kdewQj/4BAXFGlvWwy2+b4oZSz8lh9Z7+40g90lk1+X+kYyyAMxw+2fBJtjVcLxH9UeVzFuwn2wIDAQABAoGAT/cHpbVz+YNxB46TGyaWSbt0c4kzq6layLeFWBomv74UEIXnOUKjFHhgNzx14/J9hulKBOG+N44+DFa8iJSkToC709vGC50+A18hpzAFjEgd9iRASri4nDy5WLtp/Z0UXN95GOmN2wpqmqujXP1mNjMT2q+opkZO9FiVn6MAUVkCQQDv3mlSgdh0URagcGIdUSwW3tABNH+Ej/+Yt3aL4n2c2tnMGdvYTZZYdAoGc1k6u+leTePDzQ86Y1V7hfYUEA7/AkEAwsQg6JOJlFlCzQOZZQU1STB2gwMpHrFII/8BBa49VNpXtYEf2ftSiIrUwmukrUToRtnEz7DpE8Pty1G2nJMDJQJBAK17l4MT5CAkMZAyF9QKTC/yUFA+zudqWjrynauIUL8sTY9fOHCVlOI+cq29qVBgbeVFwjBv086v3zhqZ9KU1rMCQAkZjMgRDJ6HxR07C9Gyepje0MqyPRuYANzdrziKuYbbZLmwPMK8gVCr2+DxkpId5BRbXFyv0VQBFX0oZjXOkJkCQQCW+Vwx7/nK8kp77LvMqgU/QVDweBhjkIclDcKG3we5XPUpr8yFTQLux9uo8doiPBu2jlmZX4JiRghKvC1/zU+h\n-----END RSA PRIVATE KEY-----"
+private_key = 
 
-public_key = "-----BEGIN PUBLIC KEY-----\nMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDDI6d306Q8fIfCOaTXyiUeJHkrIvYISRcc73s3vF1ZT7XN8RNPwJxo8pWaJMmvyTn9N4HQ632qJBVHf8sxHi/fEsraprwCtzvzQETrNRwVxLO5jVmRGi60j8Ue1efIlzPXV9je9mkjzOmdssymZkh2QhUrCmZYI/FCEa3/cNMW0QIDAQAB\n-----END PUBLIC KEY-----"
+public_key = 
 
-notify_url = "www.youknowwho.com.cn"
+notify_url = 
 
 ali = alipay(APPID,private_key,public_key,notify_url)
 
